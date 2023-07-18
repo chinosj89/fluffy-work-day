ceee49c932780ca793db83a015a7a460b529610e
raphael.sanjuan13@gmail.com <raphael.sanjuan13@gmail.com>
2023-07-18 15:43:17 -0700

    final commit for 7/18/2023

--- Develop/script.js
+++ Develop/script.js
@@ -1,50 +1,52 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-var description = document.querySelector('.description');
 //Current Date
 var currentDay = dayjs();
-$('#currentDay').text(currentDay.format('[Today is] dddd MMMM D YYYY'));
+$('#currentDay').text(currentDay.format('[Today is:] dddd MMMM D, YYYY'));
 
 let currentHour = parseInt(dayjs().format('HH'));
 //I had learned that parseINT returns an integer. using this in my code i can compare numerically the time in 24hr format to the time block with the numerical id's that have a split so side by side integer comparison can be done
 
+
+//
 $(function () {
   // Each div has a saveBtn and this code calls those buttons
-  $('.saveBtn').on("click", function(event){
+  $('.saveBtn').on("click", function (event) {
     event.preventDefault();
     //window.alert('Saved in Local Storage!');
-    $(".notification").removeClass("d-none");
+    $(".notification").removeClass("d-none"); //in vanilla JS we used notification.classList.remove()
+
     // this selects the input field
-    var input = $(this).siblings(".description").val(); 
+    var input = $(this).siblings(".description").val();
     // var input = $(".time-block").children(".description").val(); - can also be done this way
-    var time = $(this).parent().attr("id"); // (this) refers to the button,`.parent` will call the parent div while the attr('id') will call to the id of the div
-    localStorage.setItem(time, input); // furthermore we can now associate the input itself with the div's id. 
-    //Append a text here that saved text is in local storage
-    
+
+    var time = $(this).parent().attr("id"); // (this) refers to the button,`.parent` will call the parent div while the attr('id') will call to the id of the div.
+
+    // furthermore we can now associate the input itself with the div's id. 
+    localStorage.setItem(time, input); //save 
+
+
     // Mock up had a notification when saved button is clicked and i wanted to copy that. I created an h2 with a display: none; then when the button is clicked it will remove the class. I then learned how to set a timeout where a class is added in a second which is display:none. This shows the h2 and after a second it add display none.
-    setTimeout(function(){
-      $(".notification").addClass("d-none")
-    },1000);
-  
+    setTimeout(function () {
+      $(".notification").addClass("d-none") //vanilla js is notification.classList.add()
+    }, 1000);
+
   });
 
-// Uses the time block class div and parses the div id into an integer while splitting the '-' away, while the [1] is accesing the 9.
-console.log("current time in HH: " + currentHour);
-$('.time-block').each(function(){
-  var timeBlock = parseInt($(this).attr("id").split("-")[1]);
-  console.log("timeBlocks: " + timeBlock);
-  if (timeBlock === currentHour ) {
-    $(this).addClass('present');
-  } else if (timeBlock > currentHour) {
-    $(this).addClass('future');
-  } else if (timeBlock < currentHour) {
-    $(this).addClass('past');
-  }
-})
+  // Uses the time block class div and parses the div id into an integer while splitting the '-' away, while the [1] is accesing the 9.
+  console.log("current time in HH: " + currentHour);
+  $('.time-block').each(function () {
+    var timeBlock = parseInt($(this).attr("id").split("-")[1]);
+    console.log("timeBlocks: " + timeBlock);
+    if (timeBlock === currentHour) {
+      $(this).addClass('present');
+    } else if (timeBlock > currentHour) {
+      $(this).addClass('future');
+    } else if (timeBlock < currentHour) {
+      $(this).addClass('past');
+    }
+  })
 
- 
-// local storage
+
+  // local storage
   $("#hour-9 .description").val(localStorage.getItem("9AM"));
   $("#hour-10 .description").val(localStorage.getItem("10AM"));
   $("#hour-11 .description").val(localStorage.getItem("11AM"));
@@ -54,6 +56,6 @@ $('.time-block').each(function(){
   $("#hour-15 .description").val(localStorage.getItem("3PM"));
   $("#hour-16 .description").val(localStorage.getItem("4PM"));
   $("#hour-17 .description").val(localStorage.getItem("5PM"));
-  
+
 
 });
--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# Work Day Scheduler Starter Code
+# Work Day Scheduler: Week 5 Challenge
+
+## URL: 
+
+## Description: 
+As a student, we are to create a simple calendar application that allows a user to save events for each hour of a typical working day (9am–5pm). <br />
+
+## User Story
+`AS AN employee with a busy schedule` <br /> 
+`I WANT to add important events to a daily planner` <br /> 
+`SO THAT I can manage my time effectively` <br /> 
+
+## Acceptance Criteria
+![alt](/images/Screenshot%202023-07-18%20150806.png)<br /> 
+
+### Process
+- We were given a starter code with TODOs where our code will be added. <br />
+My first course of action was to study the HTML and CSS and how it was coded using bootstrap and to furthermore be able to connect both to JavaScript. <br />
+
+- I started with the first acceptance criteria which was to create the display for the day. <br />
+`var currentDay = dayjs();
+$('#currentDay').text(currentDay.format('[Today is:] dddd MMMM D, YYYY'));` 
+
+- Next is to create the divs for each of the time blocks from 9am to 5pm. Which in this case was simple because all I did was copy and paste 1 div. Each div is then renamed based on the hour. 
+
+- After creating the divs, I had to make sure each button is working and corresponding to the JavaScript. One of the important pieces that I learned was using `$(this)` which calls on the current element in this case the `<button>` element. Another is using `.siblings` which is another way to write `.children`. After that, I learned `.parent` wchi is the same as `.parentElement`. Overall, learning these new ways to write in JS using APIs was new to me and also in some ways convenient. 
+
+### Conditional Statements
+The conditional statements is the next thing I worked on. This allowed me to create statements to match the `currentHour` with the `time-block`. I had to be able to compare both somehow. I learned from other developer's similar assignment how they were able to compare both, but I also ran across `parseInt`. `parseInt` made it so that `currentHour` and `time-block` both returned as a whole number/integer. But what I was able to take from other developers is the code line of: <br />
+`($(this).attr("id").split("-")[1]);`
+- `$(this)` - refered to the element
+- `attr("id")` - called to the id of the element and in this case `<div id="hour-9" class="row time-block">`
+- `split` with the specifics to remove the "-" made it so that the id will return in a sub-array of `(hour, 9)` 
+- [1] - since we are trying to compare the hour # to the current time, the [1] made sure to call on the 2nd object in the sub-array which is the 9 and since we have a parseInt, we are now parsing this 9 to a whole number or integer.
+<br />
+We are now able to compare both `currentHour` and `time-block` to create conditions while adding the CSS for `past`, `present`, and `future`.
+
+### Local Storage
+Since we have used local storage in the past assignment, I was able to use it again and futher reinforce how it works. 
+- `var input` is where user information is placed that is why we have a `val();`
+- `var time` is where button is being called upon in a condition that the button is clicked, the input is saved using `localStorage.setItem`. `time` being the key and the `input` being the value. 
+
+### Extra
+I also added a display where it says "Saved in Local Storage" that shows up and disappears after a second when the saved button is clicked. This was awesome to see as it also reinforces back a previous lesson in JavaScript where a setInterval was used. In this case, setTimeout was used. 
+
+### References
+I was able to reference public github accounts of developers that also had a similar assignment. Studying their codes allowed me to learn how to best use Third-party apis but making sure that I first understand how it would work in vanilla JavaScript before implementing in my code. I have futher commented for future purposes the different code for vanilla js and the api. 
